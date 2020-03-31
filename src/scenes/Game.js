be6f13e1c419ec7afe5fd00ac376d1b39fe360be
R.R. Nederhoed <rr@nederhoed.com>
2020-04-01 01:07:56 +0200

    Added a map using the Tiled map editor

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -14,6 +14,10 @@ class Game extends Phaser.Scene {
   init(data) {}
 
   preload() {
+
+    this.load.tilemapTiledJSON('level-1', 'assets/tilemaps/level-1.json');
+    this.load.image('world-1-sheet', 'assets/tilesets/spritesheet_ground-32x32.png');
+
     this.load.spritesheet('unicorn-idle-sheet', 'assets/unicorn/idle.png', {
       frameWidth: 240,
       frameHeight: 240,
@@ -60,30 +64,47 @@ class Game extends Phaser.Scene {
       frameRate: 8,
       repeat: -1,
     });
-    this.hero = new Unicorn(this, 0, 0);
+
+    this.addMap();
+    this.addHero(120, 20);
 
     this.bullets = new Bullets(this);
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    // Scenery
-    var platform = this.add.rectangle(30, 120, 60, 5, 0xffffff);
-    this.physics.add.existing(platform, true);
-    this.physics.add.collider(this.hero, platform);
+    // // Scenery
+    // var platform = this.add.rectangle(30, 120, 60, 5, 0xffffff);
+    // this.physics.add.existing(platform, true);
+    // this.physics.add.collider(this.hero, platform);
 
-    platform = this.add.rectangle(430, 120, 60, 5, 0xffffff);
-    this.physics.add.existing(platform, true);
-    this.physics.add.collider(this.hero, platform);
-    platform = this.add.rectangle(120, 60, 5, 120, 0xffffff);
-    this.physics.add.existing(platform, true);
-    this.physics.add.collider(this.hero, platform);
-    platform = this.add.rectangle(400, 60, 5, 120, 0xffffff);
-    this.physics.add.existing(platform, true);
-    this.physics.add.collider(this.hero, platform);
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.cameras.main.startFollow(this.hero);
+  }
+
+  addMap() {
+    this.map = this.make.tilemap({key: 'level-1'});
+    const groundTiles = this.map.addTilesetImage('world-1', 'world-1-sheet');
+    const groundLayer = this.map.createStaticLayer('Ground', groundTiles);
+    groundLayer.setCollision([9, 89, 44, 13, 21, 60], true);
+
+    // XXX: DEBUGGING
+    // const debugGraphic = this.add.graphics();
+    // groundLayer.renderDebug(debugGraphic);
+
+    this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.physics.world.setBoundsCollision(true, true, true, true);
+    console.log('Map added');
+  }
+
+  addHero(x, y) {
+    this.hero = new Unicorn(this, x, y);
+    this.physics.add.collider(
+      this.hero, this.map.getLayer('Ground').tilemapLayer);
   }
 
   update(time, delta) {
     if (Phaser.Input.Keyboard.JustDown(this.cursors.down)) {
       console.log('FIRE!!');
+      console.log(this.hero.body.x);
       this.bullets.fireBullet(this.hero.x, this.hero.y);
     }
 
