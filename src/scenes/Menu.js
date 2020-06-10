bb67bb4f4814dbaff9eae8aaed2d469b64b9bcbf
Jed Hastwell <jed@whistlefire.com>
2020-06-10 12:34:38 +0200

    Display top scores on the menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,16 +18,16 @@ class Menu extends Phaser.Scene {
     const itemsPerRow = 4;
     for (let i = 0; i < levels.length; i ++) {
       const unlocked = i <= gameState.maxUnlockedLevel();
-      const button = this.add.text(
-        80 + (i % itemsPerRow) * 150, 
-        140 + Math.floor(i / itemsPerRow) * 120, 
-        levels[i].name, 
-        {
-          font: '30px Arial',
-          fill: '#000000',
-        }
-      );
+      const x = 80 + (i % itemsPerRow) * 150;
+      const y = 140 + Math.floor(i / itemsPerRow) * 120;
+      const button = this.add.text(x, y, levels[i].name, { font: '30px Arial', fill: '#000000' });
       button.alpha = unlocked ? 1 : 0.5;
+
+      const highScore = gameState.getHighScore(i);
+      if (highScore !== undefined) {
+        this.add.text(x, y + 35, `Top Score: ${highScore}`, { font: '12px Arial', fill: '#000000' });
+      }
+
       if (unlocked) {
         button.setInteractive();
         // When menu button is clicked, switch to game scene and pass along the level to load.
